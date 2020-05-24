b2a78f50ed20c0c39ff3de786a325ec07c402eb1
Flunt1k <47900126+Flunt1k@users.noreply.github.com>
2020-05-24 19:14:53 +0300

    Changed rules for eslint

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const prod = process.env.NODE_ENV === 'production'
+const dev = !prod
+
+const fileName = extension => dev ?
+    `bundle.${extension}` :
+    `bundle.[hash].${extension}`
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  ]
+
+  if (dev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
+
+
+const path = require('path')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: fileName('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'core': path.resolve(__dirname, 'src/core')
+    }
+  },
+  devtool: dev ? 'source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: dev
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: prod,
+        collapseWhitespace: prod
+      }
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist')}
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: fileName('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: dev,
+              reloadAll: true
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders()
+      }
+    ]
+  }
+}
